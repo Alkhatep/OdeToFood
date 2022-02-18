cf720b7d3f85d25f0b4ac1fdb70cd00c6de2a090
Alkhatep <abdulla.sayed94@gmail.com>
2022-02-18 03:43:46 +0200

    Bulding a data Access service

---
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,32 @@
+﻿using OdoToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Vinnys", Location = "Maadi", Cuisine = CuisineType.Italian},
+                new Restaurant { Id = 2, Name = "Sizzler", Location = "Nasr city", Cuisine = CuisineType.Mexican},
+                new Restaurant { Id = 3, Name = "Mahraga", Location = "Maadi", Cuisine = CuisineType.Indian},
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}

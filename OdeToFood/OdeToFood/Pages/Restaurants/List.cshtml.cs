ff5dd758499ce20f8624bd9e60f12bb235064a5b
Alkhatep <abdulla.sayed94@gmail.com>
2022-02-20 01:01:20 +0200

    Using Model Binding and Tag Helpers

--- OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -18,15 +18,18 @@ namespace OdeToFood.Pages.Restaurants
         public string Message { get; set; }
         public IEnumerable<Restaurant> Restaurants { get; set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerm { get; set; }
+
         public ListModel(IConfiguration config, IRestaurantData restaurantData)
         {
             this.config = config;
             this.restaurantData = restaurantData;
         }
-        public void OnGet(string searchTerm)
+        public void OnGet()
         {
             Message = config["Message"];
-            Restaurants = restaurantData.GetRestauransByName(searchTerm);
+            Restaurants = restaurantData.GetRestauransByName(SearchTerm);
         }
     }
 }

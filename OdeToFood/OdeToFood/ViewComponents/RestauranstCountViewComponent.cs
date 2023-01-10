69cff05b6a533c249788b8634706be9f13af3fd6
Alkhatep <abdulla.sayed94@gmail.com>
2023-01-10 19:59:17 +0200

    Implementing a ViewComponent

---
+++ OdeToFood/OdeToFood/ViewComponents/RestauranstCountViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Data;
+
+namespace OdeToFood.ViewComponents
+{
+    public class RestauranstCountViewComponent
+        : ViewComponent
+    {
+        private readonly IRestaurantData restaurantData;
+
+        public RestauranstCountViewComponent(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var count = restaurantData.GetCountOfRestaurants();
+            return View(count);
+        }
+    }
+}
